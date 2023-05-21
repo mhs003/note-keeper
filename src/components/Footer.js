672edd07d254171fa7242bc0e613bc3f7e3245f3
Monzurul Hasan <49614333+mhs003@users.noreply.github.com>
2023-05-21 14:37:37 +0600

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer className="footer fixed bottom-0 w-full footer-center p-4 bg-base-100 text-base-content border-t border-t-base-200 shadow-2xl bg-opacity-50 backdrop-blur">
             <div>
-                <p>Copyright <a target="_blank" rel="noopener noreferrer" className="link link-secondary" href="https://github.com/mhs003">Monzurul Hasan</a> | Project source available in <a target="_blank" rel="noopener noreferrer" href="https://github.com/mhs003/note-keeper/">Github</a></p>
+                <p>Copyright <a target="_blank" rel="noopener noreferrer" className="link link-secondary" href="https://github.com/mhs003">Monzurul Hasan</a> | Project source available in <a target="_blank" rel="noopener noreferrer" className="link link-primary" href="https://github.com/mhs003/note-keeper/">Github</a></p>
             </div>
         </footer>
     )
