2cad2045ba09309ca627adf0c171bfc858648b4c
Monzurul Hasan <49614333+mhs003@users.noreply.github.com>
2023-05-21 11:41:07 +0600

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const Footer = () => {
-    const currentYear = new Date().getFullYear();
+    // const currentYear = new Date().getFullYear();
     return (
         <footer className="footer fixed bottom-0 w-full footer-center p-4 bg-base-100 text-base-content border-t border-t-base-200 shadow-2xl bg-opacity-50 backdrop-blur">
             <div>
-                <p>Copyright © {currentYear} - All right reserved by <a className="link link-secondary" href="https://github.com/mhs003">Monzurul Hasan</a></p>
+                <p>Copyright <a target="_blank" rel="noopener noreferrer" className="link link-secondary" href="https://github.com/mhs003">Monzurul Hasan</a> | Project source available in <a target="_blank" rel="noopener noreferrer" href="https://github.com/mhs003/note-keeper/">Github<a></p>
             </div>
         </footer>
     )
